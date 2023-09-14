d46430b1d9c471864780f93f6fb933eda6f7a910
JulioFlores <juliobolivianojc17@gmail.com>
2023-09-14 07:43:11 -0400

    first commit

---
+++ src/hangman/HangManComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { KeyBoardComponent } from "./components/KeyBoardComponent";
+export const HangManComponent = () => {
+  return (
+    <div>
+      <h1>HangMan</h1>
+      <hr />
+
+<KeyBoardComponent />
+    </div>
+  );
+};
+
---
+++ src/hangman/components/KeyBoardComponent.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const KeyBoardComponent = () => {
+  return (
+    <div>
+      <h1>KeyBoardComponent</h1>
+    </div>
+  );
+};
